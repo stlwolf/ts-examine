2d4bb3a6992c2f3935810b451882eeabc0a34b2a
eddy <eddy@stlwolf.com>
2023-01-30 18:22:51 +0900

    fix: Renamed val

--- src/index.ts
+++ src/index.ts
@@ -28,26 +28,26 @@ class Failure<T = ErrorPayload> {
     }
 }
 
-type MutationResult<T = undefined, S = ErrorPayload> =
+type Result<T = undefined, S = ErrorPayload> =
     | Success<T>
     | Failure<S>;
 
 // 省略なし構文。OK。
-const mutation: MutationResult<boolean> = new Success<boolean>(true);
+const mutation: Result<boolean> = new Success<boolean>(true);
 // 型引数推論が走るから大丈夫。OK。
-const mutation2: MutationResult<boolean> = new Success(true);
+const mutation2: Result<boolean> = new Success(true);
 // Failureの型はデフォルト引数があるので省略してもエラーにはならない。OK。
-const mutation3: MutationResult<boolean> = new Failure({ messages: ['test message'] });
+const mutation3: Result<boolean> = new Failure({ messages: ['test message'] });
 
 // 型判定が変わってエラーになるようになった？？
 // でも確かに普通に構文解釈すると<true>はおかしい。NG。
-const mutation4: MutationResult<true> = new Success(true);
+const mutation4: Result<true> = new Success(true);
 
-// こっちは通る。Failureの時はMutationResultの第一型引数は利用しないから？？OK。
-const mutation5: MutationResult<true> = new Failure({ messages: ['test message'] });
+// こっちは通る。Failureの時はResultの第一型引数は利用しないから？？OK。
+const mutation5: Result<true> = new Failure({ messages: ['test message'] });
 // これも上記と同じ。第一型引数は使わないから関係ないのか？。OK
-const mutation6: MutationResult = new Failure({ messages: ['test message'] });
+const mutation6: Result = new Failure({ messages: ['test message'] });
 
 // NG。どうも`true`は型引数推論してくれない？booleanにならない？
-const mutation7: MutationResult = new Success(true);
+const mutation7: Result = new Success(true);
 
